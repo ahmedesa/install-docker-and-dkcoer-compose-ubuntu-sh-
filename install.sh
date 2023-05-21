323dcabe0542867956f06b955b7d68be4f5fc657
Ahmed Esa <ahmedesa6120@gmail.com>
2023-05-21 11:45:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "--------------------Installing Docker--------------------"
 sudo apt update -y 
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -7,8 +8,10 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update -y
 sudo apt install docker-ce -y
 
+echo "--------------------Installing Docker-compose--------------------" 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+echo "--------------------Add Docker to Sudo group--------------------"
 sudo groupadd docker
 sudo usermod -aG docker $USER
